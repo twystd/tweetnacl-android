4b11793ae7e8e4299859975b637cc038ed0fb96c
Tony Seebregts <AnthonyS@rmcs.co.za>
2014-06-25 16:50:45 +0200

    Fixed crash in randombytes

--- TODO.md
+++ TODO.md
@@ -2,3 +2,7 @@
 
 1. JNI: zero out arrays and only populate if rc == 0
 2. Change to u8 etc instead of unsigned char
+3. Remove the pre-padding in ciphertext and message (?)
+4. Hmmm - check keypair implemtation: slides say it should be pk = crypto_box_keypair(&sk)
+   (Ref. http://cryptojedi.org/peter/data/tenerife-20130121.pdf)
+5. Revert license to public domain (github ?)
--- native-lib/jni/devurandom.c
+++ native-lib/jni/devurandom.c
@@ -7,32 +7,28 @@
 
 static int fd = -1;
 
-void randombytes(unsigned char *x,unsigned long long xlen) {
-    int i  = 0;
+void randombytes(unsigned char *x,unsigned long long xlen)
+{
+  int i;
 
+  if (fd == -1) {
     for (;;) {
       fd = open("/dev/urandom",O_RDONLY);
-
-      if (fd != -1)
-    	  break;
-
+      if (fd != -1) break;
       sleep(1);
     }
+  }
 
   while (xlen > 0) {
-    if (xlen < 1048576)
-    	i = xlen;
-    else
-    	i = 1048576;
+    if (xlen < 1048576) i = xlen; else i = 1048576;
 
     i = read(fd,x,i);
-
     if (i < 1) {
       sleep(1);
       continue;
     }
 
-    x    += i;
+    x += i;
     xlen -= i;
   }
 }
