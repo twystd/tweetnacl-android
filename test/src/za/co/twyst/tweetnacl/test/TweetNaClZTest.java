c1271a7e01946dde1fb83c4391a7244697c69d05
tonys <tony.seebregts@gmail.com>
2014-10-29 16:36:06 +0200

    tweetnacl/z: implemented crypto_box

---
+++ test/src/za/co/twyst/tweetnacl/test/TweetNaClZTest.java
@@ -0,0 +1,77 @@
+package za.co.twyst.tweetnacl.test;
+
+import java.util.Random;
+
+import android.test.AndroidTestCase;
+
+import za.co.twyst.tweetnacl.TweetNaClZ;
+
+public abstract class TweetNaClZTest extends AndroidTestCase {
+    // CONSTANTS
+
+    protected static final String TAG = "TweetNaCl";
+    protected static final int    ROUNDS = 100; // TODO change to 10000 when done
+    
+    private static final char[]   HEX = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                          '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' 
+                                        };
+
+    // TEST VARIABLES
+
+    protected TweetNaClZ tweetnaclz;
+    protected Random     random;
+
+    // SETUP/TEARDOWN
+
+    @Override
+    protected void setUp() throws Exception {
+        tweetnaclz = new TweetNaClZ();
+        random     = new Random();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        tweetnaclz.release();
+    }
+
+    // UTILITY FUNCTIONS
+
+    /**
+     * Converts a byte array to a hexadecimal string.
+     * 
+     */
+    protected static String tohex(byte[] bytes) {
+        char[] string = new char[bytes.length * 2];
+        int ix = 0;
+
+        for (byte x : bytes) {
+            string[ix++] = HEX[(x >> 4) & 0x0f];
+            string[ix++] = HEX[(x >> 0) & 0x0f];
+        }
+
+        return new String(string);
+    }
+
+    /**
+     * Converts a hexadecimal string to a byte array. This function
+     * intentionally crashes with an ArrayIndexOutOfRange if the string length
+     * isn't a multiple of 2.
+     * <p>
+     * And yes, using a Map lookup isn't efficient.
+     */
+    protected static byte[] fromhex(String string) {
+        char[] chars = string.toCharArray();
+        byte[] bytes = new byte[chars.length / 2];
+        int i = 0;
+        int ix = 0;
+
+        while (i < chars.length) {
+            int msb = Character.digit(chars[i++], 16);
+            int lsb = Character.digit(chars[i++], 16);
+
+            bytes[ix++] = (byte) (((msb << 4) & 0x00f0) | (lsb & 0x000f));
+        }
+
+        return bytes;
+    }
+}
